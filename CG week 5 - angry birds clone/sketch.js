01001617322c7f75e7c7e45ecff215944c86519b
kriskoss <64606271+kriskoss@users.noreply.github.com>
2023-06-28 19:48:26 +0200

    angry birds - score added , random box number.

--- CG week 5 - angry birds clone/sketch.js
+++ CG week 5 - angry birds clone/sketch.js
@@ -12,25 +12,69 @@ var MouseConstraint = Matter.MouseConstraint;
 
 var engine;
 var propeller;
-var boxes = [];
-var birds = [];
-var colors = [];
+var boxes;
+var birds;
+var colors;
 var ground;
 var slingshotBird, slingshotConstraint;
-var angle=0;
-var angleSpeed=0;
+var angle;
+var angleSpeed;
 var canvas;
 
 // MY GLOBAL VARIABLES
 var maxAngleSpeed = 0.4;
 var birdSize = 20;
-var boxSize = 80
+var boxSize;
 var slingshotBirdInitPos;
-
 var Events = Matter.Events
+var score;
+var towerHeight;
+var towerWidth;
+
+
+var ifGameOver;;
 ////////////////////////////////////////////////////////////
 function setup() {
   canvas = createCanvas(1000, 600);
+
+  birdBoxCollisionDetector = new birdBoxCollisionDetector();
+  initializeGame();
+}
+
+////////////////////////////////////////////////////////////
+function draw() {
+  
+  Engine.update(engine);
+  
+  background(0);
+
+  drawGround();
+
+  drawPropeller();
+
+  drawTower();
+
+  drawBirds();
+
+  drawSlingshot();
+
+  drawScore();
+
+  checkForGameOver();
+
+  
+}
+////////////////////////////////////////////////////////////
+function initializeGame(){
+  boxes = [];
+  birds = [];
+  colors = [];
+  score = -3;
+  angle=0;
+  angleSpeed=0;
+  boxSize= random(50,80)
+  towerHeight = random(6,8);
+  towerWidth = random(3,5);
 
   engine = Engine.create();  // create an engine
 
@@ -43,101 +87,16 @@ function setup() {
   setupSlingshot();
 
   setupMouseInteraction();
-
-  setupCollisionDetections();
-}
-
-function shouldSplit(bodyA,bodyB){
   
-  if ((bodyB.label == "slingshotBird" && bodyA.label =="towerBox") ){
-    // Check the collision speed threshold here
-    const collisionSpeedThreshold = 15; // Adjust this value as per your needs
-    const relativeVelocity = Matter.Vector.sub(bodyA.velocity, bodyB.velocity);
-    const collisionSpeed = Matter.Vector.magnitude(relativeVelocity);
-    console.log("MUST SPLIT");
-    return collisionSpeed >= collisionSpeedThreshold;
-  }
-  else if(bodyB.label == "slingshotBird" && bodyA.label =="smallBox"){
-    console.log("SPLIT SMALL!!!!")
-  }
-  else{
-    return false
-  }
+  birdBoxCollisionDetector.setup();
 }
 
-function splitBody(bodyA){
-  
-  
-}
-
-function removeFromBoxes(body) {
-  let index = boxes.indexOf(body); // gets the index of the box in the boxes array
-  
-  if (index !== -1) { // box exists in the boxes array
-    boxes.splice(index, 1); // removes box from the boxes array
-    colors.splice(index, 1); // removes colour from the colours array
-  }
-}
-
-function replaceWithStack(originalBox) {
-  // Get the position and velocity of the original box
-  const position = originalBox.position;
-  const velocity = originalBox.velocity;
-  const angularVelocity = originalBox.angularVelocity;
-
-  // Remove the original box from the world
-  Matter.World.remove(engine.world, originalBox);
-  removeFromBoxes(originalBox);
-
-  // Create a stack of rectangles
-  const stackWidth = 3; // Width of the stack (number of rectangles)
-  const stackHeight = 3; // Height of the stack (number of rectangles)
-  const stackSize = boxSize / stackWidth; // Size of each rectangle in the stack
-  const stack = [];
-  for (let i = 0; i < stackHeight; i++) {
-    for (let j = 0; j < stackWidth; j++) {
-      const x = position.x + j * stackSize;
-      const y = position.y + i * stackSize;
-      const rectangle = Bodies.rectangle(x, y, stackSize, stackSize,{label:"smallBox"});
-      stack.push(rectangle);
-    }
-  }
-
-  // Add the stack to the world
-  Matter.World.add(engine.world, stack);
-
-  // Adjust the position and velocity of each rectangle in the stack
-  stack.forEach((rectangle, index) => {
-    const x = position.x + ((index % stackWidth) - 0.5) * stackSize;
-    const y = position.y + (Math.floor(index / stackWidth) - 0.5) * stackSize;
-    Body.setPosition(rectangle, { x, y });
-    Body.setVelocity(rectangle, velocity);
-    Body.setAngularVelocity(rectangle, angularVelocity);
-  });
-
-  // Update the boxes and colors arrays with the new rectangles
-  boxes = boxes.concat(stack);
-  colors = colors.concat(Array(stack.length).fill(boxColor));
-}
-////////////////////////////////////////////////////////////
-function draw() {
-  background(0);
-
-  Engine.update(engine);
-
-  drawGround();
-
-  drawPropeller();
-
-  drawTower();
-
-  drawBirds();
-
-  drawSlingshot();
-}
-////////////////////////////////////////////////////////////
 //use arrow keys to control propeller
 function keyPressed(){
+  if (keyCode == ENTER){
+    restartGame();
+    
+  }
   if (keyCode == LEFT_ARROW){
     // Decrease angle speed and go counetr-clockwise
     if (angleSpeed > -maxAngleSpeed){
@@ -166,6 +125,11 @@ function keyTyped(){
   }
 }
 
+function restartGame(){
+  initializeGame()
+
+  
+}
 
 //////////////////// MY CODE ///////////////////////
 function setupPropeller(){
@@ -191,7 +155,7 @@ function drawPropeller(){
 
 function setupBird(){
   // creates a bird
-  let bird = Bodies.polygon(mouseX, mouseY, 1, birdSize,{restitution:0.8, friction:0.5});
+  let bird = Bodies.polygon(mouseX, mouseY, 1, birdSize,{restitution:0.8, friction:0.5, label:"bird"});
   Body.setMass(bird,1)
   
   // keeps track of the bird object
@@ -214,6 +178,7 @@ function drawBirds(){
       removeFromWorld(birds[i]);
       birds.splice(i,1);
       i--;
+      score++;
     }
   }
 }
@@ -221,11 +186,11 @@ function drawBirds(){
 
 function setupTower(){
   // Creating tower elements
-  for (let i=0;i<6;i++){    // tower height in boxes
-    for (let j=0;j<3;j++){  // tower width in boxes
+  for (let i=0;i<towerHeight;i++){    // tower height in boxes
+    for (let j=0;j<towerWidth;j++){  // tower width in boxes
       
       // Creatng single box
-      let box = Bodies.rectangle(width*0.7+j*boxSize, 620-i*boxSize, boxSize,boxSize, {label:"towerBox"})
+      let box = Bodies.rectangle(width*0.95-j*boxSize, 620-i*boxSize, boxSize,boxSize, {label:"towerBox"})
       boxes.push(box);
       World.add(engine.world,[box])
       
@@ -250,6 +215,7 @@ function drawTower(){
       boxes.splice(i,1);
       colors.splice(i,1)
       i--
+      score++
     }
   }
 }
@@ -289,27 +255,183 @@ function drawSlingshot(){
 }
 
 
-function setupCollisionDetections(){
-  Events.on(engine, 'collisionStart', function(event) {
-    let pairs = event.pairs;
-    
-    // Loop over the pairs of objects that collided
-    for (let i = 0; i < pairs.length; i++) {
-      let bodyA = pairs[i].bodyA;
-      let bodyB = pairs[i].bodyB;
-
-      // Check if bodyA should be split
-      if (shouldSplit(bodyA,bodyB)) { 
-                // Remove the original body from the world
-        Matter.World.remove(engine.world, bodyA); 
-        removeFromBoxes(bodyA)
-        replaceWithStack(bodyA)
-      }
-    }
-  });
+function drawScore(){
+  // Shows how many boxes remain do be destroyed and total number of destroyed boxes (the score)
+  push()
+  // TO DESTROY
+  textSize(26);
+  fill("red");
+  text("To destroy: " + boxes.length, 10, 30);
+  
+  // SCORE
+  textSize(32);
+  fill("yelow");
+  text("Score: " + score, 10, 60);
+  
+  // DUST, SMALLBOX, TOWERBOX REMAINING
+  textSize(20);
+  fill(125);
+  // show only boxes which label is "smallBox"
+  let towerBoxes = boxes.filter(box => box.label == "towerBox");
+  let smallBoxes = boxes.filter(box => box.label == "smallBox");
+  let dusts = boxes.filter(box => box.label == "dust");
+  text("TowerBoxes: " +towerBoxes.length + ", SmallBoxes: "+smallBoxes.length + ", Dust: "+dusts.length,270, 30);
+  pop()
 }
 
+function checkForGameOver(){
+  // noloop if no boxes left
+  if (boxes.length == 0){
+    // display game over message
+    push()
+    textSize(70);
+    fill('orange');
+    text("GAME OVER", width/2-150, height/2);
+    
+    // display the final score
+    textSize(30);
+    fill('green');
+    text("Boxes destroyed: " + score, width/2-150, height/2+50);
+    fill(125);
+    textSize(20);
+    text("Press ENTER to restart", width/2-150, height/2+100)
+    pop()
+    
+    // stop the game
+    ifGameOver = true;
+  }
 
+  if (ifGameOver == false){
+    drawScore();
+  }
+}
+
+class birdBoxCollisionDetector{
+  constructor(){
+    self = this; // self has to be used as the Events on has its own this
+  }
+  
+  setup(){
+    Events.on(engine, 'collisionStart', function(event) {
+      let pairs = event.pairs;
+      
+      // Loop over the pairs of objects that collided
+      for (let i = 0; i < pairs.length; i++) {
+        let bodyA = pairs[i].bodyA;
+        let bodyB = pairs[i].bodyB;
+  
+        // Check if bodyA should be split
+        if (self.shouldSplit(bodyA,bodyB)) { 
+        
+          // Remove the original body from the world
+          Matter.World.remove(engine.world, bodyA); 
+          
+          self.removeFromBoxes(bodyA)
+          score++;
+          if (bodyA.label != "dust"){
+            self.replaceWithStack(bodyA)
+          };
+        };
+      };
+    });
+  }
+
+  checkSpeedThreshold(bodyA,bodyB,minSpd){
+    /// Check if the collision speed is above a threshold to generate split
+    let collisionSpeedThreshold = minSpd; 
+    let relativeVelocity = Matter.Vector.sub(bodyA.velocity, bodyB.velocity);
+    let collisionSpeed = Matter.Vector.magnitude(relativeVelocity);
+    return collisionSpeed >= collisionSpeedThreshold;
+  }
+
+  shouldSplit(bodyA,bodyB){
+    /// Check if body hit by a bird should split
+    if (bodyB.label == "slingshotBird" || bodyB.label == "bird"){
+      if (bodyA.label =="towerBox"){ // tower box hit by bird
+        if (self.checkSpeedThreshold(bodyA,bodyB,25)){
+          console.log("TOWER BOX" + bodyA.id + "SPLIT!");
+          return true;
+        }
+      }
+      /// Chcks if small box hit by bird should split
+      else if(bodyA.label =="smallBox"){ // small box hit by bird
+        if (self.checkSpeedThreshold(bodyA,bodyB,15)){
+          console.log("SMAlL BOX" + bodyA.id + "SPLIT!");
+          return true;
+        }
+      }
+      else if(bodyA.label =="dust"){ // dust hit by bird
+        if (self.checkSpeedThreshold(bodyA,bodyB,5)){
+          console.log("DUST" + bodyA.id + "destroy!");
+          return true;
+        }
+      }
+    }
+    
+    else{
+      return false;
+    }
+  }
+
+  removeFromBoxes(body) {
+    let index = boxes.indexOf(body); // gets the index of the box in the boxes array
+    
+    if (index !== -1) { // box exists in the boxes array
+      boxes.splice(index, 1); // removes box from the boxes array
+      colors.splice(index, 1); // removes colour from the colours array
+    }
+  }
+
+  replaceWithStack(originalBox) {
+    // Get the position and velocity of the original box
+    let position = originalBox.position;
+    let velocity = originalBox.velocity;
+    let angularVelocity = originalBox.angularVelocity;
+
+    // Remove the original box from the world
+    Matter.World.remove(engine.world, originalBox);
+    self.removeFromBoxes(originalBox);
+
+    // Create a stack of rectangles
+    
+    let stackWidth = 3; // Width of the stack (number of rectangles)
+    let stackHeight = 3; // Height of the stack (number of rectangles)
+    
+    let stackSize = boxSize / stackWidth; // Size of each rectangle in the stack
+    if (originalBox.label == "smallBox"){
+      stackSize = stackSize/3;
+    }
+    let stack = [];
+    for (let i = 0; i < stackHeight; i++) {
+      for (let j = 0; j < stackWidth; j++) {
+        let x = position.x + j * stackSize;
+        let y = position.y + i * stackSize;
+        let label  = "smallBox"
+        if (originalBox.label == "smallBox"){
+          label = "dust"
+        }
+        let rectangle = Bodies.rectangle(x, y, stackSize, stackSize,{label:label});
+        stack.push(rectangle);
+      }
+    }
+    
+    // Add the stack to the world
+    Matter.World.add(engine.world, stack);
+
+    // Adjust the position and velocity of each rectangle in the stack
+    stack.forEach((rectangle, index) => {
+      let x = position.x + ((index % stackWidth) - 0.5) * stackSize;
+      let y = position.y + (Math.floor(index / stackWidth) - 0.5) * stackSize;
+      Body.setPosition(rectangle, { x, y });
+      Body.setVelocity(rectangle, velocity);
+      Body.setAngularVelocity(rectangle, angularVelocity);
+    });
+
+    // Update the boxes and colors arrays with the new rectangles
+    boxes = boxes.concat(stack);
+    colors = colors.concat(Array(stack.length).fill(boxColor));
+  }
+}
 
 //**********************************************************************
 //  HELPER FUNCTIONS - DO NOT WRITE BELOW THIS line
